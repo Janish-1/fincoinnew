28f9cb1be850fc8a543294d9ee42f9817aecaeff
Janish-1 <janish.pancholi11@gmail.com>
2024-02-01 17:30:28 +0530

    done back handler

--- App.js
+++ App.js
@@ -1,25 +1,48 @@
-// HomeScreen.js
-import React, { useRef } from "react";
-import {
-  View,
-  SafeAreaView,
-  StyleSheet,
-} from "react-native";
+import React, { useRef, useEffect } from "react";
+import { View, SafeAreaView, StyleSheet, BackHandler, ToastAndroid } from "react-native";
 import WebView from "react-native-webview";
 
 const HomeScreen = () => {
   const webViewRef = useRef(null);
-  
+  let lastBackPressed = 0;
+
+  const handleBackButton = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+
+    const currentTime = new Date().getTime();
+
+    if (currentTime - lastBackPressed < 2000) {
+      // If back button is pressed twice within 2 seconds, exit the app
+      BackHandler.exitApp();
+      return false;
+    } else {
+      ToastAndroid.show("Press back again to exit", ToastAndroid.SHORT);
+      lastBackPressed = currentTime;
+      return true;
+    }
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBackButton);
+
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleBackButton);
+    };
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.container}>
-          <WebView
-            ref={webViewRef}
-            source={{ uri: "https://fincoin.swastikcredit.in/" }}
-            style={styles.webview}
-            onError={(error) => console.error("WebView error:", error)}
-          />
-        </View>
+      <View style={styles.container}>
+        <WebView
+          ref={webViewRef}
+          source={{ uri: "https://fincoin.swastikcredit.in/" }}
+          style={styles.webview}
+          onError={(error) => console.error("WebView error:", error)}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -30,9 +53,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   webview: {
-    flex: 1,
-  },
-  flex: {
     flex: 1,
   },
 });
